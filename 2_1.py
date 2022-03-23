3c43344bfa51fe176d61706dc00ceaf3689167ea
Michael Ogezi <okibeogezi@outlook.com>
2022-03-23 08:17:14 -0600

    chore: completed 2a and 2b

--- 2_1.py
+++ 2_1.py
@@ -1,9 +1,11 @@
 import json
 import sys
+import xml.etree.ElementTree as ET
 
 dat_pth = sys.argv[1]
 align_pth = sys.argv[2]
 out_path = sys.argv[3]
+rag_path = 'raganato/en.blind.xml'
 
 if len(sys.argv) < 4:
     print('Invalid command-line arguments')
@@ -12,8 +14,12 @@ if len(sys.argv) < 4:
 dat_tups = [i.strip().split(' ||| ') for i in open(dat_pth, 'r').readlines()]
 align = list(map(lambda x: [tuple([int(j) for j in i.split('-')])
              for i in x.strip().split(' ')], open(align_pth, 'r').readlines()))
+rag = ET.parse(rag_path)
+sents_xml = rag.getroot()[0]
 
 assert len(dat_tups) == len(align)
+assert len(dat_tups) == len(sents_xml)
+
 combos = {}
 punks = set([',', '.', '?', '(', ')', ':', '"', "'", ';', '-', '--'])
 
@@ -24,6 +30,14 @@ for index in range(len(dat_tups)):
     combo = []
     s_to_t = {}
     t_to_s = {}
+
+    for i_index, t_word in enumerate(t):
+        t_word = t_word.lower()
+        x_text = sents_xml[index][i_index].text.lower()
+        if (t_word.replace('\'', '') in x_text) or (x_text.replace('\'', '') in t_word) or (t_word == 'n\'t' and x_text == 'not'):
+            pass
+        else:
+            assert t_word == x_text
 
     for s_i, t_i in a:
         try:
@@ -42,10 +56,13 @@ for index in range(len(dat_tups)):
             print('Line: {}, (S, T) Index: {},'.format(index, (s_i, t_i)))
             raise e
 
-    for k, v in list(s_to_t.items()):
+    for _, (k, v) in enumerate(list(s_to_t.items())):
         combo_map = {}
         combo_map['src'] = s[k]
         combo_map['tgt'] = [t[v_] for v_ in v]
+        combo_map['lemma'] = [sents_xml[index][v_].attrib['lemma'] for v_ in v]
+        combo_map['id'] = [sents_xml[index][v_].attrib['id'] if (
+            sents_xml[index][v_].tag == 'instance') else None for v_ in v]
 
         # source is a punctuation
         if s[k] in punks:
